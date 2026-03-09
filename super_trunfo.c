d9e8da118e24bed32d29221adbccf2e5293dcab5
becamaiia <s4lliemaia@gmail.com>
2026-03-09 11:18:23 -0300

    Super_trunfo.c

---
+++ super_trunfo.c
@@ -0,0 +1,26 @@
+#include <stdio.h>
+int main() {
+  char estado[65];
+char cidade[96];
+int populacao;
+float area;
+float pib;
+int pontosTuristicos;
+printf("Cadastro de carta - Super Trunfo\n");
+printf("Digite o estado: ");
+scanf("%s", estado);
+printf("Digite a cidade: ");
+scanf("%s", cidade);
+printf("Digite a populacao: ");
+scanf("%d", &populacao);
+printf("Digite a area: ");
+scanf("%f", &area);
+printf("Digite o PIB: ");
+scanf("%f", &pib);
+printf("Digite o numero de pontos turisticos: ");
+scanf("%d", &pontosTuristicos);
+printf("\nCarta cadastrada!\n);
+  printf("Estado: %s\n", estado);
+  printf("Cidade: %s\n", cidade);
+return 0;
+}
